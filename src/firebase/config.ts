16ab3a2f1159f169a4baedea71f8294a8d26d426
Naga Sheshadri <nagasheshadri001@gmail.com>
2025-12-13 01:57:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-937319871-8640a",
+  "appId": "1:955456569110:web:911720fa6a5ddf2abb5a37",
+  "apiKey": "AIzaSyB3YjIi8GCC-2p40IFoJEGDfpOGrQlUNxk",
+  "authDomain": "studio-937319871-8640a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "955456569110"
+};
